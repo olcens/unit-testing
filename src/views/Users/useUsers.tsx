0a39788248fa122b4c52c48c751636a68f608d2a
Olcen <szymon.olcen@neoteric.eu>
2022-08-02 21:09:43 +0200

    chore: add a comment regarding dispatch type

--- src/views/Users/useUsers.tsx
+++ src/views/Users/useUsers.tsx
@@ -5,7 +5,7 @@ import { getFetchedUsers, getAreUsersFetching } from 'store/users/selectors';
 import { AppDispatch } from 'store/store';
 
 export const useUsers = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>(); // TODO -- put dispatch type inside redux config
   const users = useSelector(getFetchedUsers);
   const isLoading = useSelector(getAreUsersFetching);
 
