ce237c0eea40eeedb78f1ece00d8b3ee30f524dd
Olcen <szymon.olcen@neoteric.eu>
2022-09-01 09:18:48 +0200

    chore: create test blocks for other cases

--- src/__test__/views/Users/Users.test.tsx
+++ src/__test__/views/Users/Users.test.tsx
@@ -1,6 +1,11 @@
 import { Users } from 'views/Users';
 import { renderWithProviders } from '__test__/renderWithProviders';
 
+// 1. Did list render
+// 2. Did list render with a proper user count
+// 3. Did (UserCard) onClick trigger a (useUsers) function
+// 4. Did list crash if API returns an error
+
 describe('Users view', () => {
   it('should display a list of users', async () => {
     const { getByText, findAllByText } = renderWithProviders(<Users />);
@@ -9,4 +14,8 @@ describe('Users view', () => {
     const nameNode = await findAllByText(/Name/i);
     expect(nameNode).toBeTruthy();
   });
+
+  it('should display a list with quantity of requested resource', async () => {});
+  it('click on user card should trigger a function', async () => {});
+  it('should not crash when API returns an error', async () => {});
 });
