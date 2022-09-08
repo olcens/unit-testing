4906de6436471c7f64c73299644385c0090a5c6e
Olcen <szymon.olcen@neoteric.eu>
2022-09-08 10:14:16 +0200

    *: add a test to happen

--- src/__test__/views/Users/Users.test.tsx
+++ src/__test__/views/Users/Users.test.tsx
@@ -9,12 +9,24 @@ import { UserApiResponse } from 'types/User/user';
 // 2. Did list render with a proper user count ✅
 // 3. Did (UserCard) onClick trigger a (useUsers) function
 // 4. Did list crash if API returns an error ✅
-// 5. Did loading disappeared after data is fetched
+// 5. Did loading disappeared after data is fetched ✅
 
 vi.mock('axios');
 const mockedAxios = axios as Mocked<typeof axios>;
 
 describe('Users view', () => {
+  it('should hide loader if the data is fetched', async () => {
+    const { getByText, findAllByText } = renderWithProviders(<Users />);
+    expect(getByText(/Loading.../i).textContent).toBe('Loading...');
+
+    try {
+      await findAllByText(/Loading.../i);
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+
   it('should display a list of users', async () => {
     const USERS_COUNT = 5;
     const mockedResponse: Pick<AxiosResponse<UserApiResponse>, 'data'> = {
@@ -26,8 +38,7 @@ describe('Users view', () => {
     };
     mockedAxios.get.mockResolvedValue(mockedResponse);
 
-    const { getByText, findAllByTestId } = renderWithProviders(<Users />);
-    expect(getByText(/Loading.../i).textContent).toBe('Loading...');
+    const { findAllByTestId } = renderWithProviders(<Users />);
 
     const nameNodes = await findAllByTestId('user-card');
     expect(nameNodes).toBeTruthy();
@@ -35,14 +46,14 @@ describe('Users view', () => {
   });
 
   it('click on user card should trigger a function', async () => {
-    const { getByText, findAllByText } = renderWithProviders(<Users />);
+    const { getByText, findAllByTestId } = renderWithProviders(<Users />);
+    const userCardNodes = await findAllByTestId('user-card');
   });
 
   it('should not crash when API returns an error', async () => {
     mockedAxios.get.mockRejectedValue({});
-    const { getByText, findAllByTestId } = renderWithProviders(<Users />);
+    const { findAllByTestId } = renderWithProviders(<Users />);
 
-    expect(getByText(/Loading.../i).textContent).toBe('Loading...');
     try {
       await findAllByTestId('user-card');
       // Should not happen
